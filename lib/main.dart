b85f42f0622813f762355f5422da3a9de04726bf
runcycle <justinbartholomew@gmail.com>
2019-05-21 16:31:10 -0400

    Starting setting up layout in Main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,19 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(
-    MaterialApp(
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.teal,
-        body: Container(),
+        body: SafeArea(
+          child: Row(
+            
+          ),
+        ),
       ),
-    ),
-  );
+    );
+  }
 }
